29ea055a7da84e584784936ce2068072e58a12ac
Facundo Saldivia <facundosaldivia@gmail.com>
2021-11-03 20:26:59 -0300

    subi lista y arboles

---
+++ arbol/arbol_test.py
@@ -0,0 +1,49 @@
+# Entrega de los ejercicios 5, 16, 23 solo entregar el link del repositorio donde están los archivos resueltos. PAG 158
+
+from arbol import Arbol
+from random import randint
+
+# Dado un árbol con los nombre de los superhéroes y villanos de la saga Marvel Cinematic Universe (MCU), desarrollar un algoritmo que contemple lo siguiente:
+
+# a. además del nombre del superhéroe, en cada nodo del árbol se almacenará un campo booleano que indica si es un héroe o un villano, True y False respectivamente;
+# b. listar los villanos ordenados alfabéticamente;
+# c. mostrar todos los superhéroes que empiezan con C;
+# d. determinar cuántos superhéroes hay el árbol;
+# e. Doctor Strange en realidad está mal cargado. Utilice una búsqueda por proximidad para encontrarlo en el árbol y modificar su nombre;
+# f. listar los superhéroes ordenados de manera descendente;
+# g. generar un bosque a partir de este árbol, un árbol debe contener a los superhéroes y otro a los villanos, luego resolver las siguiente tareas:
+  # I. determinar cuántos nodos tiene cada árbol;
+  # II. realizar un barrido ordenado alfabéticamente de cada árbol.
+
+# arbol = Arbol()
+
+
+# superheroe = {'name': 'Doctor Strnge', 'villano': False, 'aparicion': 1942}
+# arbol = arbol.insertar_nodo(superheroe['name'], superheroe)
+# superheroe = {'name': 'Capitan America', 'villano': False, 'aparicion': 1942}
+# arbol = arbol.insertar_nodo(superheroe['name'], superheroe)
+# superheroe = {'name': 'Capitana Marvel', 'villano': False, 'aparicion': 1942}
+# arbol = arbol.insertar_nodo(superheroe['name'], superheroe)
+# superheroe = {'name': 'Docasdasdas', 'villano': False, 'aparicion': 1942}
+# arbol = arbol.insertar_nodo(superheroe['name'], superheroe)
+# superheroe = {'name': 'Iron Man', 'villano': False, 'aparicion': 1942}
+# arbol = arbol.insertar_nodo(superheroe['name'], superheroe)
+# superheroe = {'name': 'Iron Hulk', 'villano': False, 'aparicion': 1942}
+# arbol = arbol.insertar_nodo(superheroe['name'], superheroe)
+
+
+# print(arbol.inorden())
+
+# b. listar los villanos ordenados alfabéticamente;
+# def ver_villanos(arbol):
+#     if (arbol.info is not None):
+#         ver_villanos(arbol.izq)
+#     if not arbol.datos['esHeroe']:
+#         print(arbol.info)
+#     if (arbol.der is not None):
+#         ver_villanos(arbol.der)
+# ver_villanos(arbol_mcu)
+
+
+
+
